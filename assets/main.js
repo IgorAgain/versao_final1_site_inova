a2cc52a8c5e89ff59528c864733d1d400f539749
IgorAgain <igorpetrindealmeida@gmail.com>
2025-08-29 17:56:46 -0300

    descrição da mudança

--- assets/main.js
+++ assets/main.js
@@ -202,9 +202,9 @@ function animateStats() {
         
         function formatNumber(num) {
             if (num >= 1000000) {
-                return (num / 1000000).toFixed(1).replace('.0', '') + 'M';
-            } else if (num > 1000) {
-                return '1';
+                return (num / 1000000).toFixed(1).replace('.0', '') + ' MM';
+            } else if (num == 100000) {
+                return '100' + ' M';
             }
             return num.toString();
         }
